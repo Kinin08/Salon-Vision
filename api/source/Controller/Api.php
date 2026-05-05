912b69ec4a25e7f9bb267f917f3ffddb7cf1ffab
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-05-05 09:03:11 -0300

    Atualização da classe Api

--- api/source/Controller/Api.php
+++ api/source/Controller/Api.php
@@ -10,4 +10,26 @@ class Api
         echo "Olá, mundo! Estamos com a API funcionando, graças a Deus!";
     }
 
+    protected function call (int $code, ?string $status = null, ?string $message = null, ?string $type = null): \Source\WebService\Api
+    {
+        if(!empty($status)){
+            $this->response = [
+                "code" => $code,
+                "type" => $type,
+                "status" => $status,
+                "message" => (!empty($message) ? $message : null)
+            ];
+        }
+        return $this;
+    }
+
+    protected function back(?array $data = null): Api
+    {
+        if ($data) {
+            $this->response["data"] = $data;
+        }
+        echo json_encode($this->response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        return $this;
+    }
+
 }
