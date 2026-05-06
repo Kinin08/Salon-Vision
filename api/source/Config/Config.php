03a67a234f9a0e670926a04f38d4d3ff8ea6c571
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-05-06 08:39:59 -0300

    API - Lista de Produtos - GET

--- api/source/Config/Config.php
+++ api/source/Config/Config.php
@@ -2,3 +2,10 @@
 
 const CONF_URL_BASE = "http://localhost:8080/acme-3am"; // URL base do site, geralmente localhost em desenvolvimento
 const CONF_URL_TEST = "http://localhost:8080/acme-3am"; // URL base do site, geralmente localhost em desenvolvimento
+
+
+const CONF_DB_HOST = "mysql";
+const CONF_DB_NAME = "db-acme-manha";
+const CONF_DB_USER= "root";
+const CONF_DB_PORT = 3306;
+const CONF_DB_PASS= "1234567";
--- api/source/Controller/Products.php
+++ api/source/Controller/Products.php
@@ -3,13 +3,16 @@
 namespace source\Controller;
 
 use Source\Controller\Api;
+use Source\Models\Product;
 
 class Products extends Api
 {
 
-    public function productsList ()
+    public function productsList (): void
     {
-        echo "Lista de produtos";
+        $product = new Product();
+        $response = $product->listAll();
+        $this->call("200", "success", "Lista de produtos", "success")->back($response);
     }
 
 }
