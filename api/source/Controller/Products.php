6b2b870e2ca5ce58d6b98a809c23e684f24de292
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-05-06 09:32:35 -0300

    API - Lista de Produtos - GET

--- api/source/Controller/Products.php
+++ api/source/Controller/Products.php
@@ -8,11 +8,5 @@ use Source\Models\Product;
 class Products extends Api
 {
 
-    public function productsList (): void
-    {
-        $product = new Product();
-        $response = $product->listAll();
-        $this->call("200", "success", "Lista de produtos", "success")->back($response);
-    }
 
 }
